106af57e803eec35c9437517c7acd8e3d61b98eb
Brian McAllister <brian@brianmcallister.com>
2019-11-13 17:45:18 -0500

    Remove demo server.

--- demo/workers/worker.ts
+++ demo/workers/worker.ts
@@ -1,8 +1,8 @@
 import { WorkerCore } from '../../dist';
 import { Resource } from '../src/types';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const worker = new WorkerCore<Resource>({
+// eslint-disable-next-line no-new
+new WorkerCore<Resource>({
   url: 'http://localhost:3000',
   interval: 5000,
 });
