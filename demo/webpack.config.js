62535d0d904d2971fdadc7bed63242fb9d6e7d28
Brian McAllister <brian@brianmcallister.com>
2019-11-14 10:37:58 -0500

    Remove unused packages.

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
